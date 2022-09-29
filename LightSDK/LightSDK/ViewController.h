20390d7e06f57dbf9728c9d4e48f1a5b63bbfc43
xiaoqi <58425129+yuyu202209@users.noreply.github.com>
2022-09-29 17:02:43 +0800

    Add files via upload

---
+++ LightSDK/LightSDK/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  LightSDK
+//
+//  Created by lunawye on 2022/9/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LightSDK/LightSDK/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LightSDK
+//
+//  Created by lunawye on 2022/9/29.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
